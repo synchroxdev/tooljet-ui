4c8ff24196e159674388f1e7c458e94a18d81591
JoseCabrera99 <josecabrera9914@gmail.com>
2023-08-02 14:57:14 -0500

    Base del circularprogress

--- frontend/src/Editor/Components/CirularProgressbar.jsx
+++ frontend/src/Editor/Components/CirularProgressbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import config from 'config';
+import { Box } from '@mui/material';
+import { CircularProgress } from '@mui/joy';
 
 export const CircularProgressBar = function CircularProgressBar({ height, properties, styles, dataCy }) {
   const { text, progress } = properties;
@@ -12,26 +15,44 @@ export const CircularProgressBar = function CircularProgressBar({ height, proper
   };
 
   return (
-    <div style={computedStyles} data-cy={dataCy}>
-      <CircularProgressbar
-        value={progress}
-        text={text}
-        styles={{
-          root: {
-            height: height,
-          },
-          path: {
-            stroke: color,
-          },
-          text: {
-            fill: textColor,
-            fontSize: textSize,
-          },
-        }}
-        strokeWidth={strokeWidth}
-        counterClockwise={counterClockwise}
-        circleRatio={circleRatio}
-      />
-    </div>
+    <>
+      {config.UI_LIB === 'mui' && (
+        <div style={computedStyles} data-cy={dataCy}>
+          <CircularProgressbar
+            value={progress}
+            text={text}
+            styles={{
+              root: {
+                height: height,
+              },
+              path: {
+                stroke: color,
+              },
+              text: {
+                fill: textColor,
+                fontSize: textSize,
+              },
+            }}
+            strokeWidth={strokeWidth}
+            counterClockwise={counterClockwise}
+            circleRatio={circleRatio}
+          />
+        </div>
+      )}
+      {config.UI_LIB === 'x' && (
+        <Box style={computedStyles}>
+          <CircularProgress
+            value={progress}
+            determinate
+            sx={{
+              '& .MuiCircularProgress-progress': {
+                stroke: color,
+                strokeWidth: strokeWidth,
+              },
+            }}
+          />
+        </Box>
+      )}
+    </>
   );
 };
