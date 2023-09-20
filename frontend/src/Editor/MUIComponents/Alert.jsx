bdc5633038bf2f8c59995ee3d59311221386a00b
Fredy Gil <fgil666@gmail.com>
2023-09-19 20:10:06 -0500

    new Alert component

---
+++ frontend/src/Editor/MUIComponents/Alert.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { Alert as MUIAlert, AlertTitle } from '@mui/material';
+
+export const Alert = function Alert({ properties, styles, registerAction, setExposedVariable, dataCy, fireEvent }) {
+  const [severity, setSeverity] = useState();
+  const [title, setTitle] = useState(() => computeText());
+  const [text, setText] = useState(() => computeText());
+  const [visibility, setVisibility] = useState(styles.visibility);
+  const [variant, setVariant] = useState(styles.variant);
+
+  useEffect(() => {
+    if (visibility !== styles.visibility) setVisibility(styles.visibility);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [styles.visibility]);
+
+  useEffect(() => {
+    if (variant !== styles.variant) setVariant(styles.variant);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [styles.variant]);
+
+  useEffect(() => {
+    if (severity !== properties.severity) setSeverity(properties.severity);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [properties.severity]);
+
+  useEffect(() => {
+    setTitle(properties.title);
+    setExposedVariable('title', title);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [properties.title]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    const text = computeText();
+    setText(text);
+    setExposedVariable('text', text);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [properties.text]);
+
+  registerAction(
+    'setText',
+    async function (text) {
+      setText(text);
+      setExposedVariable('text', text);
+    },
+    [setText]
+  );
+  registerAction(
+    'visibility',
+    async function (value) {
+      setVisibility(value);
+    },
+    [setVisibility]
+  );
+
+  function computeText() {
+    return properties.text === 0 || properties.text === false ? properties.text?.toString() : properties.text;
+  }
+
+  const computedStyles = {
+    display: visibility ? 'flex' : 'none',
+    boxShadow: styles.boxShadow,
+  };
+
+  return (
+    <div data-cy={dataCy}>
+      <MUIAlert
+        severity={severity}
+        variant={variant}
+        style={computedStyles}
+        onClick={() => {
+          fireEvent('onClick');
+        }}
+        onMouseOver={() => {
+          fireEvent('onHover');
+        }}
+      >
+        {title ? <AlertTitle>{title}</AlertTitle> : null}
+        {text}
+      </MUIAlert>
+    </div>
+  );
+};
--- frontend/src/Editor/WidgetManager/customWidgetConfig.js
+++ frontend/src/Editor/WidgetManager/customWidgetConfig.js
@@ -160,4 +160,112 @@ export const customWidgets = [
       },
     },
   },
+  {
+    name: 'Alert',
+    displayName: 'Alert',
+    description: 'Display alert messages',
+    component: 'Alert',
+    defaultSize: {
+      width: 20,
+      height: 50,
+    },
+    others: {
+      showOnDesktop: { type: 'toggle', displayName: 'Show on desktop' },
+      showOnMobile: { type: 'toggle', displayName: 'Show on mobile' },
+    },
+    properties: {
+      title: {
+        type: 'code',
+        displayName: 'Title',
+        validation: {
+          schema: { type: 'string' },
+        },
+      },
+      text: {
+        type: 'code',
+        displayName: 'Text',
+        validation: {
+          schema: { type: 'string' },
+        },
+      },
+      severity: {
+        type: 'select',
+        displayName: 'Severity',
+        options: [
+          { name: 'Error', value: 'error' },
+          { name: 'Info', value: 'info' },
+          { name: 'Success', value: 'success' },
+          { name: 'Warning', value: 'warning' },
+        ],
+        validation: {
+          schema: { type: 'string' },
+        },
+      },
+    },
+    events: {
+      onClick: { displayName: 'On click' },
+      onHover: { displayName: 'On hover' },
+    },
+    styles: {
+      variant: {
+        type: 'select',
+        displayName: 'Variant',
+        options: [
+          { name: 'Standard', value: 'standard' },
+          { name: 'Filled', value: 'filled' },
+          { name: 'Outlined', value: 'outlined' },
+        ],
+        validation: {
+          schema: { type: 'string' },
+        },
+      },
+      visibility: {
+        type: 'toggle',
+        displayName: 'Visibility',
+        validation: {
+          schema: { type: 'boolean' },
+          defaultValue: false,
+        },
+      },
+    },
+    exposedVariables: {
+      buttonText: 'Button',
+    },
+    actions: [
+      {
+        handle: 'click',
+        displayName: 'Click',
+      },
+      {
+        handle: 'setText',
+        displayName: 'Set Text',
+        params: [{ handle: 'text', displayName: 'Text', defaultValue: 'New Text' }],
+      },
+      {
+        handle: 'disable',
+        displayName: 'Disable',
+        params: [{ handle: 'disable', displayName: 'Value', defaultValue: `{{false}}`, type: 'toggle' }],
+      },
+      {
+        handle: 'visibility',
+        displayName: 'Visibility',
+        params: [{ handle: 'visible', displayName: 'Value', defaultValue: `{{false}}`, type: 'toggle' }],
+      },
+    ],
+    definition: {
+      others: {
+        showOnDesktop: { value: '{{true}}' },
+        showOnMobile: { value: '{{false}}' },
+      },
+      properties: {
+        text: { value: `Este es un mensaje informativo - revísalo` },
+        severity: { value: 'info' },
+      },
+      events: [],
+      styles: {
+        visibility: { value: '{{true}}' },
+        variant: 'standard',
+      },
+    },
+  },
 ];
